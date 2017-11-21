84c8e44552113c0355836ae5b7dca52d27c41047
Gastalver <mgastalver@gmail.com>
2017-11-22 00:35:23 +0100

    Con HeroesComponent

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,13 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeroesComponent } from './heroes.component';
+import { FormsModule } from '@angular/forms';
 
 describe('HeroesComponent', () => {
-  let component: HeroesComponent;
-  let fixture: ComponentFixture<HeroesComponent>;
+  let component: HeroesComponent; // Angular component
+  let fixture: ComponentFixture<HeroesComponent>; // API para testear componente
+  let element: fixture.nativeElement; // DOM Element
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [ HeroesComponent ]
     })
     .compileComponents();
@@ -16,7 +18,7 @@ describe('HeroesComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeroesComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    element = fixture.nativeElement;
   });
 
   it('should create', () => {
@@ -36,12 +38,25 @@ describe('HeroesComponent', () => {
   });
 
   it('should render hero.name in a h2 tag', async(() => {
-    const fixture = TestBed.createComponent(HeroesComponent);
+    component.hero = {
+      id: 2,
+      name: 'Cagarruto'
+    };
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h2').textContent).toContain('WindStorm Details');
+    expect(element.querySelector('h2').textContent).toContain('Cagarruto'.toUpperCase());
   }));
 
+it('should display an input field',async(() => {
+  expect(element.querySelector('input')).toBeDefined()
+}));
 
+it('should render hero.name in h2 tag', async(() => {
+
+  component.hero = {
+    id: 2,
+    name: 'Cagarruta'};
+  fixture.detectChanges(); // Trigger change detection to update the DOM element.
+  expect(element.querySelector('h2').innerHTML).toContain('Cagarruta'.toUpperCase())
+  }))
 
 });
