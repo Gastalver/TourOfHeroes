b3bf619102e98ebb82dac849207f441242179057
Gastalver <mgastalver@gmail.com>
2017-11-21 02:24:46 +0100

    Commit inicial

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -35,6 +35,13 @@ describe('HeroesComponent', () => {
     expect(component.hero.name).toBeDefined();
   });
 
+  it('should render hero.name in a h2 tag', async(() => {
+    const fixture = TestBed.createComponent(HeroesComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('WindStorm Details');
+  }));
+
 
 
 });
