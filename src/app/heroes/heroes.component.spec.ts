092f62320dd1828ef7420e797e61666661d0c426
Gastalver <mgastalver@gmail.com>
2017-11-24 01:45:00 +0100

    4. Displaying a list completado. Abandono los test para centrarme en Angular.

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,11 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeroesComponent } from './heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HEROES} from '../mock-heroes';
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent; // Angular component
   let fixture: ComponentFixture<HeroesComponent>; // API para testear componente
-  //let element: fixture.nativeElement; // DOM Element
+  let element: fixture.nativeElement; // DOM Element
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,22 +23,37 @@ describe('HeroesComponent', () => {
   });
 
   it('should create', () => {
+    let fixture = TestBed.createComponent(HeroesComponent);
+    let component = fixture.componentInstance;
+    let element = fixture.nativeElement;
     expect(component).toBeTruthy();
   });
 
   it('should have a property called hero that is an object', () => {
+    let fixture = TestBed.createComponent(HeroesComponent);
+    let component = fixture.componentInstance;
+    let element = fixture.nativeElement;
     expect(component.hero).toBeDefined();
   });
 
   it('hero should have id property', () => {
+    let fixture = TestBed.createComponent(HeroesComponent);
+    let component = fixture.componentInstance;
+    let element = fixture.nativeElement;
     expect(component.hero.id).toBeDefined();
   });
 
   it('hero should have name property', () => {
+    let fixture = TestBed.createComponent(HeroesComponent);
+    let component = fixture.componentInstance;
+    let element = fixture.nativeElement;
     expect(component.hero.name).toBeDefined();
   });
 
   it('should render hero.name in a h2 tag', async(() => {
+    let fixture = TestBed.createComponent(HeroesComponent);
+    let component = fixture.componentInstance;
+    let element = fixture.nativeElement;
     component.hero = {
       id: 2,
       name: 'Cagarruto'
@@ -47,11 +63,16 @@ describe('HeroesComponent', () => {
   }));
 
 it('should display an input field',async(() => {
+  let fixture = TestBed.createComponent(HeroesComponent);
+  let component = fixture.componentInstance;
+  let element = fixture.nativeElement;
   expect(element.querySelector('input')).toBeDefined()
 }));
 
 it('should render hero.name in h2 tag', async(() => {
-
+  let fixture = TestBed.createComponent(HeroesComponent);
+  let component = fixture.componentInstance;
+  let element = fixture.nativeElement;
   component.hero = {
     id: 2,
     name: 'Cagarruta'};
